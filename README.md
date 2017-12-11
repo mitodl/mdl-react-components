07b75f0647cdaf108d099bec4bc31228102b8ad5
Nathan Levesque <nlevesq@mit.edu>
2017-12-11 14:28:07 -0500

    Configured project for publishing

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # mdl-react-components
+
+![Build Status](https://travis-ci.org/mitodl/mdl-react-components.svg?branch=master)
+
+This library provides React implementations of [material-components](https://github.com/material-components/material-components-web).
+
---
+++ rollup.config.js
@@ -0,0 +1,41 @@
+import babel from 'rollup-plugin-babel'
+
+const babelConfig = {
+  presets: [
+    ["env", {"modules": false }],
+    "react"
+  ],
+  plugins: [
+    "transform-flow-strip-types",
+    "transform-object-rest-spread",
+    "transform-class-properties",
+    "syntax-dynamic-import",
+    'external-helpers',
+    'transform-runtime'
+  ],
+  babelrc: false,
+  exclude: 'node_modules/**',
+  runtimeHelpers: true
+}
+
+const externals = [
+  '@material'
+]
+
+const sources = [
+  'index.js',
+  'Button.js',
+  'Dialog.js',
+]
+
+const config = sources.map((source) => ({
+  entry: `src/${source}`,
+  format: 'cjs',
+  dest: `lib/${source}`,
+  plugins: [
+    babel(babelConfig)
+  ],
+  external: externals
+}))
+
+export default config
